b57020641af05b9367f7ddc91666266d8c244276
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2017-10-26 16:13:19 +0200

    completed board layout

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var nrOfSquares = 8; //används för mer tydlig kod eller om du vill ändra 8
 
 var contentDiv = document.getElementById('content');
 
@@ -8,8 +9,20 @@ board.className = 'board';
 //Lägg till sist 
 contentDiv.appendChild(board);
 
-for(var y = 0; y < 8; y++) {
-    addRowToBoard();
+for(var y = 0; y < nrOfSquares; y++) {
+    var row = addRowToBoard();
+    
+    for(var x = 0; x < nrOfSquares; x++) {
+        var square = document.createElement('div');
+        square.className = 'square';
+    
+        if ( (x+y) % 2) {
+            square.style.backgroundColor = 'black';
+        } else {
+            square.style.backgroundColor = 'white';
+        }
+        row.appendChild(square);
+    }
 }
 
 function addRowToBoard () {
@@ -17,14 +30,21 @@ function addRowToBoard () {
     row.className = 'row';
     
         //t ex 1%2 går inte att dela, 1 finns kvar eller 2%2, 0 finns kvar, 3%2, 1 finns kvar 
-    if (y%2) {
-        row.style.backgroundColor = 'black';
-    } else {
-        row.style.backgroundColor = 'white';   
-    }
-    
+
+        if (y%2) {
+            row.style.backgroundColor = 'black';
+        } else {
+            row.style.backgroundColor = 'white';   
+        }
+
         board.appendChild(row);
+        return row; //skickar tillbaka row, för att komma åt row där uppe, annars säger den att row inte finns
     }
+
+    
+   
+
+
 
 
 
